72ad86cedc20390b68b44361c59827a72ff49602
Teun Willems <teun.willems+github@live.nl>
2018-01-11 16:12:37 +0100

    Scene changes async and after a while

--- Assets/Scripts/SceneChangerEmpathy.cs
+++ Assets/Scripts/SceneChangerEmpathy.cs
@@ -1,13 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace DefaultNamespace
 {
     public class SceneChangerEmpathy : MonoBehaviour
     {
-        public void ChangeToScene(string sceneName)
+        private string _sceneName;
+        
+        public void SetScene(string sceneName)
         {
-            SceneManager.LoadScene(sceneName);
+            this._sceneName = sceneName;
+        }
+
+        public void ChangeToSceneAfter(float afterTime = 2.0f)
+        {
+            StartCoroutine(this.ChangeToSceneAfterEnum(afterTime));
+        }
+
+        private IEnumerator ChangeToSceneAfterEnum(float afterTime = 2.0f)
+        {
+            var sceneAysnc = SceneManager.LoadSceneAsync(this._sceneName);
+            sceneAysnc.allowSceneActivation = false;
+            yield return new WaitForSeconds(afterTime);
+            sceneAysnc.allowSceneActivation = true;
         }
     }
 }
