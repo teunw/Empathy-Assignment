5f900fe2d91f1f35540532a056d2a5841a49cddc
Teun Willems <teun.willems+github@live.nl>
2018-01-11 11:05:04 +0100

    Added WaitForGrab

---
+++ Assets/Scripts/WaitForGrab.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using VRTK;
+
+namespace DefaultNamespace
+{
+    public class WaitForGrab : CustomYieldInstruction
+    {
+        private readonly VRTK_InteractableObject _interactableObject;
+        
+        public override bool keepWaiting
+        {
+            get { return !this._interactableObject.IsGrabbed(); }
+        }
+
+        public WaitForGrab(VRTK_InteractableObject interactable)
+        {
+            this._interactableObject = interactable;
+        }
+    }
+}
