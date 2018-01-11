95478d97543dc8ae36d578e8ad9ec423746f3641
Teun Willems <teun.willems+github@live.nl>
2018-01-11 10:18:29 +0100

    Added files from others

---
+++ Assets/Scripts/TeaSystem/Teapot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using VRTK;
+
+public class Teapot : MonoBehaviour
+{
+    public ParticleSystem ParticleSystem;
+    public AudioSource BoilingSource;
+    
+    public float HotTime = 20f;
+    public float DropTime = 2f;
+    public bool ShouldBeDropped = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (this.ParticleSystem == null) ParticleSystem = GetComponentInChildren<ParticleSystem>();
+        if (this.BoilingSource == null) BoilingSource = GetComponentInChildren<AudioSource>();
+    }
+
+    public void SetBoiling()
+    {
+        StartCoroutine(this.SetBoilingEnum());
+    }
+
+    public IEnumerator SetDropTimer(float time)
+    {
+        yield return new WaitForGrab(this.GetComponent<VRTK_InteractableObject>());
+        Debug.Log("Grabbed");
+        yield return new WaitForSeconds(time);
+        Debug.Log("Dropped");
+        DropTea();
+    }
+
+    public void DropTea()
+    {
+        var obj = this.GetComponent<VRTK_InteractableObject>();
+        obj.ForceStopInteracting();
+        obj.isGrabbable = false;
+    }
+
+    public IEnumerator SetBoilingEnum()
+    {
+        this.ParticleSystem.Play();
+
+        if (this.ShouldBeDropped) StartCoroutine(SetDropTimer(this.DropTime));
+
+        yield return new WaitForSeconds(HotTime);
+        this.ParticleSystem.Stop();
+    }
+
+    public void StartBoilSound()
+    {
+        this.BoilingSource.Play();
+    }
+
+    public void StopBoilSound()
+    {
+        this.BoilingSource.Stop();
+    }
+}
