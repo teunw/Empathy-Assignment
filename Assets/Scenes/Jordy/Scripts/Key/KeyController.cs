89624331a3053f75827615a742ae25c617ee6152
Jordy Guzak <j.guzak@student.fontys.nl>
2018-01-12 14:42:56 +0100

    VisibilityCheck script to check if certain object is visible to certain camera

---
+++ Assets/Scenes/Jordy/Scripts/Key/KeyController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class KeyController : MonoBehaviour {
+
+
+    public float minAllowedDistance = 2f;
+
+    private bool canDisappear;
+    private bool shouldDisappear;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Update()
+    {
+        if (Camera.main != null)
+            Debug.Log("Is visible: " + VisibilityCheck.IsVisible(Camera.main, gameObject));
+    }
+
+
+}
