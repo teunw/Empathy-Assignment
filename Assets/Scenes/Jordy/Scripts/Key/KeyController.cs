4b1c019d7ceabead95fd9554c346886c0527400b
Jordy Guzak <j.guzak@student.fontys.nl>
2018-01-15 10:21:56 +0100

    keymindfuck ready for testing

--- Assets/Scenes/Jordy/Scripts/Key/KeyController.cs
+++ Assets/Scenes/Jordy/Scripts/Key/KeyController.cs
@@ -2,22 +2,49 @@
 
 public class KeyController : MonoBehaviour {
 
+    [Tooltip("The new location the key should move to when out of sight.")]
+    public Transform newLocation;
 
-    public float minAllowedDistance = 2f;
+    public bool ShouldDissapear {
+        get
+        {
+            return shouldDissapear;
+        }
+        set
+        {
+            if (value)
+            {
+                shouldDissapear = true;
+                enabled = true; // enable Update() method
+            }
+            else
+            {
+                shouldDissapear = false;
+                enabled = false; // disable Update() method
+            }
+        }
+    }
 
-    private bool canDisappear;
-    private bool shouldDisappear;
+    private bool shouldDissapear;
 
 	// Use this for initialization
 	void Start () {
-		
+        ShouldDissapear = false;
 	}
+
+    public void OnGrab()
+    {
+        ShouldDissapear = true;
+    }
 
     private void Update()
     {
-        if (Camera.main != null)
-            Debug.Log("Is visible: " + VisibilityCheck.IsVisible(Camera.main, gameObject));
+        if (Camera.main != null && shouldDissapear && !VisibilityCheck.IsVisible(Camera.main, gameObject, true, false))
+        {
+            enabled = false;
+            shouldDissapear = false;
+            transform.position = newLocation.position;
+            transform.rotation = newLocation.rotation;
+        }
     }
-
-
 }
