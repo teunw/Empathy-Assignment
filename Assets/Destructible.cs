4f7725c163b52b4842e474c757548756bd0652fe
Teun Willems <teun.willems+github@live.nl>
2018-01-15 15:56:26 +0100

    Added broken teapot

---
+++ Assets/Destructible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+
+	public GameObject DestructibleObject;
+
+	public void Destroy()
+	{
+		var dest = Instantiate(this.DestructibleObject, transform);
+		dest.transform.parent = this.transform.parent;
+		Destroy(this.gameObject);
+	}
+}
