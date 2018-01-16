287cbc279534e7cde83d3b7cc193944e821b8ca8
Jordy Guzak <j.guzak@student.fontys.nl>
2018-01-16 11:09:38 +0100

    Added VRTK button which replaces lightsiwtch and tv button

---
+++ Assets/Scenes/Jordy/Scripts/Television/TelevisionController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class TelevisionController : MonoBehaviour {
+
+    private VideoPlayer tv;
+
+    private void Start()
+    {
+        tv = GetComponent<VideoPlayer>();
+    }
+
+    public void ToggleOnOff()
+    {
+        if (tv.isPlaying)
+            tv.Stop();
+        else
+            tv.Play();
+    }
+}
--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -1,49 +1,47 @@
-﻿using System;
-using UnityEngine;
-using VRTK;
-using VRTK.UnityEventHelper;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(VRTK_Button_UnityEvents))]
 public class LightSwitch : MonoBehaviour {
 
     public Light[] lights;
     private bool isOn = true;
 
-    private VRTK_Button_UnityEvents buttonEvents;
-
     // Initialize
     private void Start()
     {
         // Lights
-        for (int i = 0; i < lights.Length; i++)
-        {
-            lights[i].enabled = isOn;
-        }
-    }
-
-    private void OnEnable()
-    {
-        buttonEvents = GetComponent<VRTK_Button_UnityEvents>();
-        buttonEvents.OnPushed.AddListener(handlePush);
-    }
-
-    private void OnDisable()
-    {
-        buttonEvents.OnPushed.RemoveListener(handlePush);
-    }
-
-    private void handlePush(object sender, Control3DEventArgs e)
-    {
-        ToggleLights();
+        SetEnabled(isOn);
     }
 
     public void ToggleLights()
     {
         isOn = !isOn;
-        
+
+        SetEnabled(isOn);
+    }
+
+    public void TurnOn()
+    {
+        if (isOn) return;
+
+        isOn = true;
+
+        SetEnabled(isOn);
+    }
+
+    public void TurnOff()
+    {
+        if (!isOn) return;
+
+        isOn = false;
+
+        SetEnabled(isOn);
+    }
+
+    private void SetEnabled(bool state)
+    {
         for (int i = 0; i < lights.Length; i++)
         {
-            lights[i].enabled = isOn;
+            lights[i].enabled = state;
         }
     }
 }
