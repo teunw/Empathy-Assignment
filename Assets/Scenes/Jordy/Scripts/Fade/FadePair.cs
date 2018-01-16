4dbef611f8f0c7400ff0af7cf96fab5634a41bda
Jordy Guzak <j.guzak@student.fontys.nl>
2018-01-16 10:13:18 +0100

    re-wrote fade system

---
+++ Assets/Scenes/Jordy/Scripts/Fade/FadePair.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FadePair {
+    public Fade homeObject;
+    public Fade hospitalObject;
+}
